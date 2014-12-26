c78339792a3723b80432893cf6d7fa9451f91a47
Camille Blaser <camille.blaser@gmail.com>
2014-12-26 01:05:18 +0100

    TP4 : mockito

--- Authentification/src/test/java/fr/unilim/info/authent/InscriptionTest.java
+++ Authentification/src/test/java/fr/unilim/info/authent/InscriptionTest.java
@@ -1,9 +1,9 @@
 package fr.unilim.info.authent;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
